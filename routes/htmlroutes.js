3d6245727c5fca2fe87dceefb4676ec69832b513
Ami Asokumar <gasokumar@gmail.com>
2021-12-10 13:14:22 -0500

    added htmlroutes.js

---
+++ routes/htmlroutes.js
@@ -0,0 +1,22 @@
+// Creating a router variable/importing the express router.
+// Remember, we're organizing the routes in this router. Later, we export it so that the routes can be used.
+const router = require("express").Router();
+const path = require("path");
+
+// Will need routes for index.html, exercise.html, and stats.html
+// These will all be GET routes that will serve the static html files.
+
+router.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/index.html"));
+});
+
+router.get("/exercise", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/exercise.html"));
+});
+
+router.get("/stats", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/stats.html"));
+});
+
+// Exporting the routes so they can be used in our server.
+module.exports = router;
--- server.js
+++ server.js
@@ -2,16 +2,13 @@
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
-
 const PORT = process.env.PORT || 3000;
-
 const app = express();
+const htmlRoutes = require("./routes/htmlRoutes.js");
 
 app.use(logger("dev"));
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 app.use(express.static("public"));
 
 mongoose.connect(
@@ -24,9 +21,8 @@ mongoose.connect(
   }
 );
 
-// Make route for get /stats
-
-// Make route for get /exercise
+// HTML Routes
+app.use(htmlRoutes);
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
