2960dcd95a7113217cd7e81e5801352d847efe5f
Ami Asokumar <gasokumar@gmail.com>
2021-12-12 15:41:38 -0500

    added post and put routes for api/workouts. Also added get route for /api/workouts/range

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -14,9 +14,41 @@ app.get("/api/workouts", (req, res) => {
     });
 });
 // PUT request for /api/workouts
-
+app.put("/api/workouts/:id", (req, res) => {
+  db.Workout.findByIdAndUpdate(req.params.id, {
+    $push: {
+      exercises: req.body,
+    },
+  })
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
 // POST request for /api/workouts
-
+app.post("/api/workouts", (req, res) => {
+  db.Workout.create(req)
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
+    })
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
 // GET request for /api/workouts/range
+app.get("/api/workouts/range", (req, res) => {
+  db.Workout.find({})
+    .then((dbUser) => {
+      res.json(dbUser);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
 
 module.exports = router;
