c56411d37440e22a2ba80ccf3ff430dadb4d2d64
Ami Asokumar <gasokumar@gmail.com>
2021-12-12 14:27:05 -0500

    added workout model and exported

--- models/workout.js
+++ models/workout.js
@@ -1,3 +1,22 @@
 const mongoose = require("mongoose");
-
 const Schema = mongoose.Schema;
+// Followed form inputs from exercise.html
+const WorkoutSchema = new Schema({
+  exercise: [
+    { type: String, trim: true },
+    {
+      name: { type: String, trim: true },
+    },
+    {
+      distance: { type: Number, default: 0 },
+    },
+    { duration: { type: Number } },
+    { weight: { type: Number } },
+    { sets: { type: Number } },
+    { reps: { type: Number } },
+  ],
+});
+
+const Workout = mongoose.model("Workout", WorkoutSchema);
+
+module.exports = Workout;
